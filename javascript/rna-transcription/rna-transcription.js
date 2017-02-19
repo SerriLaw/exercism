65064c6f0e5ad85a14d69b1e4c6e235bce274ffd
Sarah Lawrence <s.j.lawrence8@gmail.com>
2017-02-19 17:46:32 +1100

    working for many char

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,18 +1,27 @@
 var DnaTranscriber = function() {};
 
-DnaTranscriber.prototype.toRna = function(x) {
-  switch (x) {
-    case 'C':
-      return 'G';
-    case 'G':
-      return 'C';
-    case 'T':
-      return 'A';
-    case 'A':
-      return 'U';
-    default:
-      throw 'Invalid input';
+DnaTranscriber.prototype.toRna = function(input) {
+  var output = '';
+  for (var i = 0; i < input.length; i++) {
+    output = output + switchLetter(input[i]);
   }
+
+  function switchLetter(x) {
+    switch (x) {
+      case 'C':
+        return 'G';
+      case 'G':
+        return 'C';
+      case 'T':
+        return 'A';
+      case 'A':
+        return 'U';
+      default:
+        throw 'Invalid input';
+    }
+  }
+
+  return output;
 };
 
 module.exports = DnaTranscriber;
