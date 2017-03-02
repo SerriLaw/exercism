4d7e2d7756f0e2b5e8b073d7003df2689e3e5f79
Sarah Lawrence <s.j.lawrence8@gmail.com>
2017-03-02 21:46:49 +1100

    gigasecond update

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,8 +1,12 @@
 var Gigasecond = function() {};
 
 Gigasecond.prototype.date = function(input) {
-  var output = '';
+  const gigasecondMS = 1000000000 * 1000;
+  var givenDate = input;
 
+  givenDate = givenDate.getTime();
+
+  var output = new Date(givenDate + gigasecondMS);
 
   return output;
 };
